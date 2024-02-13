956f32b430beb3d67c87d6bae8397c68274dee83
Amith Vardhan Reddy Surasani <132837289+amithvardhan99@users.noreply.github.com>
2024-02-13 17:41:58 -0500

    ....

--- Ethereum Price Prediction using GRU-LSTMs for Forecasting/app/model.py
+++ Ethereum Price Prediction using GRU-LSTMs for Forecasting/app/model.py
@@ -7,4 +7,10 @@ from datetime import *
 model = tf.keras.models.load_model("model.joblib")
 mms = joblib.load("scaler.joblib")
 
-cont = np.load()
+cont = np.load("closing price window and target.npz")
+
+X_train = cont["window"]
+y_train = cont["target"]
+length = cont["window_size"]
+
+print(length)
