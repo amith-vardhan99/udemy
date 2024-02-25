2d3f02a2f4b4bad5c9df92d0454af8521b655344
Amith Vardhan Reddy Surasani <132837289+amithvardhan99@users.noreply.github.com>
2024-02-24 21:32:06 -0500

    ,,,,

---
+++ Classification of Brain Tumours with CNN and OpenCV/app/app.py
@@ -0,0 +1,43 @@
+from flask import *
+import numpy as np
+import os
+from model import *
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+app = Flask(__name__)
+
+
+UPLOAD_FOLDER = "C:\\Users\\amith\\Documents\\GitHub\\udemy\\Classification of Brain Tumours with CNN and OpenCV\\app\\static"
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/', methods=['GET','POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file1' not in request.files:
+            return 'there is no file1 in form!'
+        file1 = request.files['file1']
+        path = os.path.join(app.config['UPLOAD_FOLDER'], 'output.jpg')
+        file1.save(path)
+        #return path
+        data = image_preprocessing(path)
+        s = predict_result(data)
+        if s == 1:
+            result = 'No Brain Cancer'
+        else:
+            result = 'Brain Cancer'
+    return render_template('index.html',result=result)
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
