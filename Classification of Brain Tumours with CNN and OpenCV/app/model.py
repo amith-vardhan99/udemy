45ab4dc9f84b4c3960b4e3e4f3e1092b6b7c5b3a
Amith Vardhan Reddy Surasani <132837289+amithvardhan99@users.noreply.github.com>
2024-02-24 21:25:29 -0500

    ......

---
+++ Classification of Brain Tumours with CNN and OpenCV/app/model.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+import cv2
+import warnings
+warnings.filterwarnings("ignore")
+import shutil
+from sklearn.metrics import *
+import cv2
+
+model = tf.keras.models.load_model("model.h5")
+def image_preprocessing(img_path):
+    img_org = cv2.imread(img_path)
+    img_grey = cv2.cvtColor(img_org, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(src=img_grey, dsize=(128,128))
+    img = img / 255.0
+    img = img.reshape((1,128,128,1))
+    return img
+
+def predict_result(img, model):
+    pred = int(model.predict(img,verbose=0)[0][0])
+    return pred
