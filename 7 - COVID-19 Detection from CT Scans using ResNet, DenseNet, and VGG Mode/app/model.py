c72bcd35d62528229d0a51c85c6a9a8d57dd8481
Amith Vardhan Reddy Surasani <132837289+amithvardhan99@users.noreply.github.com>
2024-03-18 18:49:57 -0500

    ......

--- 7 - COVID-19 Detection from CT Scans using ResNet, DenseNet, and VGG Mode/app/model.py
+++ 7 - COVID-19 Detection from CT Scans using ResNet, DenseNet, and VGG Mode/app/model.py
@@ -8,8 +8,7 @@ path = "C:\\Users\\amith\\Documents\\GitHub\\udemy\\7 - COVID-19 Detection from 
 model = keras.models.load_model(f'{path}\\model.h5')
 
 def image_pre(file_name):
-    image_name = path + "\\" + file_name
-    img = cv2.imread(image_name)
+    img = cv2.imread(file_name)
     img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_resized = cv2.resize(src=img_grey,dsize=(64,64))
     data = img_resized / 255.0
